ad047a76e8ede613bce04f795b5af9d8586e6d1d
slybootslion <slybootslion@vip.qq.com>
2020-01-18 00:09:30 +0800

    牛客网社区

---
+++ community/src/main/java/com/sketchmac/community/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.sketchmac.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/demo")
+public class DemoController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello, Java. Hello, Spring Boot";
+    }
+}
