485235b663a02d4849a49aa713fd71667458c08e
slybootslion <slybootslion@vip.qq.com>
2020-01-18 15:20:56 +0800

    1.30 开始

--- community/src/main/java/com/sketchmac/community/controller/DemoController.java
+++ community/src/main/java/com/sketchmac/community/controller/DemoController.java
@@ -1,8 +1,13 @@
 package com.sketchmac.community.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/demo")
@@ -13,4 +18,88 @@ public class DemoController {
     public String sayHello() {
         return "Hello, Java. Hello, Spring Boot";
     }
+
+    // GET请求
+    @RequestMapping(value = "/student", method = RequestMethod.GET)
+    @ResponseBody
+    public String getStudents(
+            @RequestParam(name = "current", required = false, defaultValue = "1") int current,
+            @RequestParam(name = "limit", required = false, defaultValue = "10") int limit
+    ) {
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(current);
+        al.add(limit);
+        return al.toString();
+    }
+
+    @RequestMapping(path = "/student/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public int getStudents(@PathVariable("id") int id) {
+        return id;
+    }
+
+    // POST请求
+    @RequestMapping(path = "/student", method = RequestMethod.POST)
+    @ResponseBody
+    public String saveStudent(String name, int age) {
+        ArrayList<String> al = new ArrayList<>();
+        al.add(name);
+        al.add(String.valueOf(age));
+        return al.toString();
+    }
+
+    // 返回模板
+    @RequestMapping(path = "/teacher", method = RequestMethod.GET)
+    public ModelAndView getTeacher() {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("name", "张三");
+        mav.addObject("age", 20);
+        mav.setViewName("/demo/view");
+        return mav;
+    }
+
+    // 返回模板方法2
+    @RequestMapping(path = "/teacher2", method = RequestMethod.GET)
+    public String getTeacher(Model model) {
+        model.addAttribute("name", "李四");
+        model.addAttribute("age", 30);
+        return "/demo/view";
+    }
+
+    // 返回json
+    @RequestMapping(path = "/emp", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, Object> getEmp() {
+        HashMap<String, Object> emp = new HashMap<>();
+        emp.put("name", "王五");
+        emp.put("age", 23);
+        emp.put("salary", 5000);
+        return emp;
+    }
+    @RequestMapping(path = "/emps", method = RequestMethod.GET)
+    @ResponseBody
+    public ArrayList<HashMap<String, Object>> getEmps() {
+        ArrayList<HashMap<String, Object>> al = new ArrayList<>();
+
+        HashMap<String, Object> emp = new HashMap<>();
+        emp.put("name", "王五");
+        emp.put("age", 23);
+        emp.put("salary", 5000);
+        al.add(emp);
+
+        emp = new HashMap<>();
+        emp.put("name", "李四");
+        emp.put("age", 22);
+        emp.put("salary", 5000);
+        al.add(emp);
+
+        emp = new HashMap<>();
+        emp.put("name", "张三");
+        emp.put("age", 21);
+        emp.put("salary", 5000);
+        al.add(emp);
+
+        return al;
+    }
+
 }
---
+++ community/src/test/java/com/sketchmac/community/MapperTest.java
@@ -0,0 +1,61 @@
+package com.sketchmac.community;
+
+import com.sketchmac.community.dao.UserMapper;
+import com.sketchmac.community.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MapperTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelectUser() {
+        User user = userMapper.selectById(102);
+        System.out.println(user);
+
+        user = userMapper.selectByName("liubei");
+        System.out.println(user);
+
+        user = userMapper.selectByEmail("nowcoder101@sina.com");
+        System.out.println(user);
+    }
+
+    @Test
+    public void testInsertUser() {
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("123456");
+        user.setSalt("abc");
+        user.setEmail("test@qq.com");
+        user.setHeaderUrl("http://www.nowcoder.com/100.png");
+        user.setCreateTime(new Date());
+
+        int row = userMapper.insertUser(user);
+        System.out.println(row);
+        System.out.println(user.getId());
+    }
+
+    @Test
+    public void updateUser() {
+        int rows = userMapper.updateStatus(150, 1);
+        System.out.println(rows);
+
+        rows = userMapper.updateHeader(150, "http://www.nowcoder.com/101.png");
+        System.out.println(rows);
+
+        rows = userMapper.updatePassword(150, "testpassword");
+        System.out.println(rows);
+
+    }
+}
